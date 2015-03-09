710a686450cd4bff50a8540c1f5ea08586b3d359
Andrew Bruce <andrew@camelpunch.com>
2015-03-09 15:20:44 +0000

    Use a regular executable instead of a Rakefile

---
+++ copy.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+require "aws-sdk"
+
+source, destination = ARGV[0..1].map { |uri| Pathname(uri) }
+puts "Copying #{source} to #{destination}"
+Aws::S3::Client.new(region: "us-east-1").
+  copy_object(bucket: destination.dirname.to_s,
+              key: destination.basename.to_s,
+              copy_source: source.to_s)
